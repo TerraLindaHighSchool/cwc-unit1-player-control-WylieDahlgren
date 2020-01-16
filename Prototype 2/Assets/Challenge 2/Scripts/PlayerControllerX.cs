bf24071414835d0517aea55896156e92c1e55e7f
Wylie Dahlgren <wd00833@srcs.loc>
2020-01-16 15:04:28 -0800

    Finished Challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogRate = 0.5f;
+    private float nextDog = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextDog)
+        {
+            nextDog = Time.time + dogRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
